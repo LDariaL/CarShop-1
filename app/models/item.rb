f985c347e8e01d566b304deb1239206587d3575c
Daria Lobanova <darialobanova911@gmail.com>
2022-10-28 23:04:04 +0300

    Add callbacks &validates to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+validates :price, numericality { greater_than: 0, allow_nil: true }
+validates :name, :description, presence: true
+
+after_initialize { p 'initialize' } # Item.new Item.first
+after_save       { p 'save'}        # Item.create
+after_create     { p 'create'}
+after_update     { p 'update' }
+after_destroy    { p 'destroy' }
+
 end
