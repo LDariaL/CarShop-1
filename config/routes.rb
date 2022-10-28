d09c01ae6ad46aa48d5e36d57cf4ae5d110787b6
Daria Lobanova <darialobanova911@gmail.com>
2022-10-29 01:56:21 +0300

    Add params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root 'application#myapp'
-  # Defines the root path route ("/")
-  # root "articles#index"
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do     
+ resources :items                        
 end
